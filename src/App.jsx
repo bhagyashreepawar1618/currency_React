57ef247942f5f2535471d367f9333f04c84f7b4b
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-01-22 18:50:39 +0530

    refactor: enhance currency converter UI and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
-import bg from "./assets/background.jpg"
 import './App.css'
+import { useState } from 'react'
+import CurValue from "./components/Curvalue.jsx"
 
 function App() {
 
+ //custom hooks 
+
+
+
+
   return (
     <>
-    <div className='h-screen w-full bg-cover bg-center' style={{backgroundImage:`URL(${bg})`}}></div>
+    <div >
+    <div className="h-100 w-150 bg-gray-400 mt-50" style={{
+      padding:'20px',
+      border:'1px solid white',
+      borderRadius:'10px'
+    }}>
+      <center><h1 className='text-black' style={{fontSize:"30px",fontWeight:"bold"}}>Currency Convertor</h1></center>
+      {/* //button for swapping values */}
+      <button style={{
+        zIndex:"2",
+        padding:"5px",
+        color:"white",
+        backgroundColor:"blueviolet",
+        fontSize:"18px",
+        fontWeight:"bold",
+        borderRadius:"5px",
+        position:"absolute",
+        marginTop:"90px",
+        marginLeft:"250px"
+      }}>Swap</button>
+      <CurValue FromOrTo="From" currencyType={Fromcur}/>   
+      <CurValue FromOrTo="To" currencyType={Tocur}/>
+      <button className='w-full h-10 text-white bg-blue-500 rounded mt-5'
+      style={{pointer:'cursor',}}>Convert {Fromcur} to {Tocur}</button>
+    </div>
+    </div>
     </>
   )
 }
--- src/components/Curvalue.jsx
+++ src/components/Curvalue.jsx
@@ -1,18 +1,39 @@
 import react from "react"
 
 // component to hold the output of changed currency
-function Curvalue(){
+function Curvalue({FromOrTo,currencyType}){
     return(
-        <div style={{display:"flex" ,
-            justifyContent:"end",
-            alignItems:"center",
-            backgroundColor:"transparent",
+        <div style={{
+            backgroundColor:"white",
             backdropFilter:"blur('10px')",
-            border:"1px solid white",
+            border:"1px solid black",
             borderRadius:"15px",
-            height:"50%",
+            height:"30%",
             width:"100%",
-        }}>hii</div>
+        }}>
+            <div style={{display:'flex',
+                justifyContent:'space-between',
+                padding:'5px'
+            }}>
+                <p>{FromOrTo}</p>
+                <p className="ms-100 text-bold">Currency Type</p>
+            </div>
+            <div style={{display:'flex',
+                justifyContent:'space-between',
+                padding:'5px'}}>
+                <p></p>
+                <select name="" id=""
+                style={{
+                    backgroundColor:"pink",
+                    border:"1px solid white",
+                    borderRadius:"5px",
+                    padding:"8px",
+                }}>
+                    <option value={currencyType}>helloo</option>
+                    <option value={currencyType}>Bhagyashree</option>
+                </select>
+            </div>
+        </div>
     )
 }
 
