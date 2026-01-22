5287023290edcfcea2a66b0f4493d6378b4f9e68
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-01-22 18:49:35 +0530

    add Curvalue component to display changed currency output

---
+++ src/components/Curvalue.jsx
@@ -0,0 +1,19 @@
+import react from "react"
+
+// component to hold the output of changed currency
+function Curvalue(){
+    return(
+        <div style={{display:"flex" ,
+            justifyContent:"end",
+            alignItems:"center",
+            backgroundColor:"transparent",
+            backdropFilter:"blur('10px')",
+            border:"1px solid white",
+            borderRadius:"15px",
+            height:"50%",
+            width:"100%",
+        }}>hii</div>
+    )
+}
+
+export default Curvalue
